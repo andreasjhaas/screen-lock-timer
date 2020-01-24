7533679ad45bce7b156ec935cba25f59dd4b92f0
Andreas Haas <andreasjhaas@gmail.com>
2020-01-24 12:15:44 +0100

    widgets revised

--- app/src/main/java/com/example/screenlocktimer/MainActivity.java
+++ app/src/main/java/com/example/screenlocktimer/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.screenlocktimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
 import android.app.admin.*;
 import android.content.*;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.*;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends AppCompatActivity {
 
     // Visuals
-    private Button btn;
+    private ToggleButton tbtn2;
     private ToggleButton tbtn;
     private TextView tv;
     private SeekBar sb;
@@ -51,28 +54,20 @@ public class MainActivity extends AppCompatActivity {
             hasStarted = true;
             try{
                 while(time > 0){
+                    Thread.sleep(60000);
+                    time --;
                     runOnUiThread(new Runnable(){
                         public void run() {
-                            if(time > 60){
-                                int s = time / 60;
-                                int m = time - 60 * s;
-                                if(s < 2){
-                                    tv.setText(s+" Stunde "+m+" min");
-                                }else{
-                                    tv.setText(s+" Stunden "+m+" min");
-                                }
-
-                            }
-                            else{
-                                tv.setText(time+" min");
-                                sb.setProgress(time);
+                            updateTimeInfo();
+                            sb.setProgress(time);
+                            if(time == 0){
+                                tbtn2.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(MainActivity.this, R.color.colorWidgets)));
+                                tbtn2.setChecked(false);
+                                sb.setEnabled(true);
                             }
                         }
                     });
-                    Thread.sleep(60000);
-                    time --;
                 }
-
                 dpm.lockNow();
 
             }catch(Exception e){
@@ -85,6 +80,79 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Methods
+    private void updateTimeInfo(){
+        time = sb.getProgress();
+        if(time > 60){
+            int s = time / 60;
+            int m = time - 60 * s;
+            if(s < 2){
+                tv.setText(s+" Stunde "+m+" min");
+            }else{
+                tv.setText(s+" Stunden "+m+" min");
+            }
+
+        } else{
+            tv.setText(time+" min");
+        }
+    }
+
+    // Listener
+    private OnCheckedChangeListener occl = new OnCheckedChangeListener(){
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            switch(buttonView.getText().toString()){
+                case "Start Timer":
+                    if (dpm.isAdminActive(cn)) {
+                        if(time != 0){
+                            sb.setEnabled(false);
+                            tt = new TimerThread(time);
+                            tt.start();
+                            tbtn2.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(MainActivity.this, R.color.colorWidgetsRed)));
+                            Toast.makeText(MainActivity.this, "Timer started", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            tbtn2.setChecked(false);
+                        }
+                    } else{
+                        tbtn2.setChecked(false);
+                        Toast.makeText(MainActivity.this, "You need to enable DeviceAdmin", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case "Stop Timer":
+                    sb.setEnabled(true);
+                    tbtn2.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(MainActivity.this, R.color.colorWidgets)));
+                    tt.interrupt();
+                    break;
+                case "OFF":
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, cn);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "ScreenLock permission");
+                    startActivityForResult(intent, RESULT_ENABLE);
+                    break;
+                case "ON":
+                    dpm.removeActiveAdmin(cn);
+                    tbtn.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(MainActivity.this, R.color.colorWidgetsRed)));
+                    break;
+            }
+        }
+    };
+
+    private OnSeekBarChangeListener oscl = new OnSeekBarChangeListener(){
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            updateTimeInfo();
+        }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,76 +161,18 @@ public class MainActivity extends AppCompatActivity {
         dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
         cn = new ComponentName(this, DeviceAdmin.class);
 
-        btn = findViewById(R.id.button);
+        tbtn2 = findViewById(R.id.toggleButton2);
         tbtn = findViewById(R.id.toggleButton);
         tv = findViewById(R.id.textView2);
         sb = findViewById(R.id.seekBar);
 
+        updateTimeInfo();
+
         tt = new TimerThread(time);
 
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                time = progress;
-                if(progress > 60){
-                    int s = progress / 60;
-                    int m = progress - 60 * s;
-                    if(s < 2){
-                        tv.setText(s+" Stunde "+m+" min");
-                    }else{
-                        tv.setText(s+" Stunden "+m+" min");
-                    }
-
-                }
-                else{
-                    tv.setText(progress+" min");
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        tbtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (tbtn.isChecked()) {
-                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, cn);
-                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "ScreenLock permission");
-                    startActivityForResult(intent, RESULT_ENABLE);
-                } else {
-                    dpm.removeActiveAdmin(cn);
-                }
-            }
-        });
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (dpm.isAdminActive(cn)) {
-                    if(tt.hasStarted()){
-                        tt.interrupt();
-                        tt = new TimerThread(time);
-                        tt.start();
-                    }else{
-                        tt = new TimerThread(time);
-                        tt.start();
-                    }
-                    Toast.makeText(MainActivity.this, "Timer started", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "You need to enable DeviceAdmin", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        sb.setOnSeekBarChangeListener(oscl);
+        tbtn.setOnCheckedChangeListener(occl);
+        tbtn2.setOnCheckedChangeListener(occl);
 
     }
 
@@ -170,6 +180,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         tbtn.setChecked(dpm.isAdminActive(cn));
+        tbtn.setBackgroundTintList(dpm.isAdminActive(cn) ? ColorStateList.valueOf(ContextCompat.getColor(MainActivity.this, R.color.colorWidgets)):ColorStateList.valueOf(ContextCompat.getColor(MainActivity.this, R.color.colorWidgetsRed)));
     }
 
     @Override
